cc2a049b4852a4435b268b0be6490a04c54a8f60
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-02-24 23:51:05 +0200

    add jsx and css to fist task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,5 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css';
 import Profile from './Profile';
-
-const userData = {
-  username: 'Jacques Gluke',
-  tag: 'jgluke',
-  location: 'Ocho Rios, Jamaica',
-  // avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+import userData from '../userData.json';
 
 const App = () => {
   return (
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,23 +1,25 @@
+import css from './Profile.module.css';
+
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.wrapper}>
+      <div className={css.userInfoWrapper}>
+        <img className={css.userPhoto} src={image} alt="User avatar" />
+        <p className={css.userName}>{name}</p>
+        <p className={css.userInfo}>@{tag}</p>
+        <p className={css.userInfo}>{location}</p>
       </div>
 
-      <ul>
-        <li>
+      <ul className={css.activityInfoWrapper}>
+        <li className={css.activitySection}>
           <span>Followers</span>
           <span>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.activitySection}>
           <span>Views</span>
           <span>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.activitySection}>
           <span>Likes</span>
           <span>{stats.likes}</span>
         </li>
